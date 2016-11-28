d48bb61e5fb1f9df074643ebac37522c1eddd9ed
i5ting <i5ting@126.com>
2016-11-28 11:13:20 +0800

    lazycommmit, please ignore

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-[[ -e ~/.profile ]] && echo "source ~/.sre" >> ~/.profile
-
-[[ -e ~/.zshrc ]] && echo "[[ -e ~/.profile ]] && emulate sh -c 'source ~/.profile' " >> ~/.zshrc
-
-echo "" > ~/.sre_path
-
-exec $SHELL
+#
+# [[ -e ~/.profile ]] && echo "source ~/.sre" >> ~/.profile
+#
+# [[ -e ~/.zshrc ]] && echo "[[ -e ~/.profile ]] && emulate sh -c 'source ~/.profile' " >> ~/.zshrc
+#
+# echo "" > ~/.sre_path
+#
+# exec $SHELL
--- tty.js
+++ tty.js
@@ -1,5 +1,10 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
 var tty = require('tty.js');
 var express = require('express')
+var userHome = require('user-home')
+var currentPath = process.cwd()
 
 var app = tty.createServer({
   shell: 'zsh',
@@ -15,4 +20,12 @@ app.get('/foo', function(req, res, next) {
 
 app.use(express.static(__dirname + '/../static'));
 
+var taskFile = __dirname +  '/static/task.json'
+if (!fs.existsSync(taskFile)) {
+  fs.writeFileSync(taskFile,  JSON.stringify({}, null, 4))
+}
+
 app.listen();
+
+
+// fs.writeFileSync(__dirname + '/app/sre.json',  JSON.stringify(all, null, 4))
